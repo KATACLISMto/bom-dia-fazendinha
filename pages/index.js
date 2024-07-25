c73938d02360dce1cc0b7ce072f77d7aec79e9e5
KATACLISMto <karloskatus@gmail.com>
2024-07-25 19:39:47 +0000

    sei la

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div>
+            <h1>NAO IREI TOMAR TODA COCA</h1>
+            <p>MAS SAIBA QUE EU TE AMO</p>
+            <p>amo muito mesmo em</p>
+            <img src="https://i.pinimg.com/474x/c0/93/2e/c0932ec6066f8273c9147a31ddcb5df2.jpg"/>
+        </div>
+    )
+}
+export default Home
